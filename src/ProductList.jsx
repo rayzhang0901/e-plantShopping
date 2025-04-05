1f8dcc146e249bbf18b4084d63a01d141a5a8753
rayzhang0901 <jingyizhang0901@gmail.com>
2025-04-04 23:19:19 -0400

    changed product.card to product-card

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -297,7 +297,7 @@ function ProductList({ onHomeClick }) {
                             <h1><div>{category.category}</div></h1>
                             <div className="product-list">
                                 {category.plants.map((plant, plantIndex) => (
-                                    <div className="product.card" key={plantIndex}>
+                                    <div className="product-card" key={plantIndex}>
                                         <img className="product-image" src={plant.image} alt={plant.name} />
                                         <div className="product-title">{plant.name}</div>
                                         <button  className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
